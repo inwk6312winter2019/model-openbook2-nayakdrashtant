b93e898c97114cdad581d8063d33d551d3a73fee
= <drahtantnayak@dal.ca>
2019-02-06 15:27:11 -0400

    Complete sub task 2 of task 1

--- task1.py
+++ task1.py
@@ -2,9 +2,18 @@ myfile = open("Street_Centrelines.csv","r")
 def dotuple():
     for f in myfile:
         f = f.split(",")
-       # f = f.strip()
         string = (f[2],f[4],f[6],f[7])
         print(string)  
         
+def maintanancehistogram():
+    mydict = dict()
+    for f in myfile:
+        f = f.split(",")
+        if f[12] not in mydict:
+            mydict[f[12]] = 1
+        else:
+            mydict[f[12]] += 1
+    print(mydict)
 
-dotuple()
+#dotuple()
+maintanancehistogram()
