9e989e35ad17753d79ebb4ecd684f6729197e189
= <drahtantnayak@dal.ca>
2019-02-06 16:36:19 -0400

    Perform sub task 4 of task1 'still need to appy some changesgit add task1.py git add task1.py '

--- task1.py
+++ task1.py
@@ -1,7 +1,8 @@
-myfile = open("Street_Centrelines.csv","r")
+#myfile = open("Street_Centrelines.csv","r")
 
 # Easy: A tuple of (STR_NAME,FULL_NAME,FROM_STR,TO_STR)
 def dotuple():
+    myfile = open("Street_Centrelines.csv","r")
     for f in myfile:
         f = f.split(",")
         string = (f[2],f[4],f[6],f[7])
@@ -11,6 +12,7 @@ def dotuple():
         
 def maintanancehistogram():
     mydict = dict()
+    myfile = open("Street_Centrelines.csv","r")
     for f in myfile:
         f = f.split(",")
         if f[12] not in mydict:
@@ -21,13 +23,48 @@ def maintanancehistogram():
 
 # List of unique owners for the streets ["OWN"]
 def listowners():
+    myfile = open("Street_Centrelines.csv","r")
     owners = []
     for f in myfile:
         f = f.split(",")
-        if f[11] not in owners:
-            owners.append(f[11])
+        f = f[11].strip()
+       # f = f.replace(" ","")
+        if f not in owners:
+            owners.append(f)
     print(owners)
 
-#dotuple()
-#maintanancehistogram()
+# Different types of Street classes ["ST_CLASS"] and a list of streets undereach class
+def getstreetclass():
+    stclass = []
+    myfile = open("Street_Centrelines.csv","r")
+    for f in myfile:
+        f = f.split(",")
+        f = f[10].strip()
+        f = f.replace(" ","")
+        if f not in stclass:
+            if len(f) >= 1:
+                stclass.append(f)
+    return stclass
+
+
+def streeclassnstreets():
+    stclass = getstreetclass()
+    myfile = open("Street_Centrelines.csv","r")
+    print(stclass)
+    my_file = open("Street_Centrelines.csv","r")
+    for st in stclass:
+        print(".",st)
+        for fm in my_file:
+            fm = fm.split(",")
+            fcm = fm[10].strip()
+            fcm = fcm.lower()
+            fcm = fcm.replace(" ","")
+            print(fm[12])
+#            if len(fcm) >= 1:
+ #               if fcm:
+  #                  print(fm[2]) 
+
+dotuple()
+maintanancehistogram()
 listowners()
+streeclassnstreets()
