65b03e1e36d5964618789a0f2a34a6947853199c
= <drahtantnayak@dal.ca>
2019-02-06 15:36:07 -0400

    Add necessary comments and complete sub task 3 of task 1

--- task1.py
+++ task1.py
@@ -1,9 +1,13 @@
 myfile = open("Street_Centrelines.csv","r")
+
+# Easy: A tuple of (STR_NAME,FULL_NAME,FROM_STR,TO_STR)
 def dotuple():
     for f in myfile:
         f = f.split(",")
         string = (f[2],f[4],f[6],f[7])
         print(string)  
+
+# A histogram of the different types of maintenance. [Result has to be a dictionary with key as Maintenance and number of streets as values] ["MAINTENANCE"]
         
 def maintanancehistogram():
     mydict = dict()
@@ -15,5 +19,15 @@ def maintanancehistogram():
             mydict[f[12]] += 1
     print(mydict)
 
+# List of unique owners for the streets ["OWN"]
+def listowners():
+    owners = []
+    for f in myfile:
+        f = f.split(",")
+        if f[11] not in owners:
+            owners.append(f[11])
+    print(owners)
+
 #dotuple()
-maintanancehistogram()
+#maintanancehistogram()
+listowners()
